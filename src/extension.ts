c6549f7bb441e2ba243a67daf48ba16439e70a37
Krzysztof Grzywocz <kgrzywocz@wp.pl>
2019-05-23 21:03:26 +0200

    code refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,39 @@
-
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('wsl-path.getRelativePath', function (uri) {
+function getActiveDocumentUri(): vscode.Uri | undefined {
 
-		if (vscode.window.activeTextEditor) {
-			uri = vscode.window.activeTextEditor.document.uri;
-		}
-		if (!uri) {
-			vscode.window.showErrorMessage('Cannot copy relative path (no file is opened?)');
-			return;
-		}
-		var path = vscode.workspace.asRelativePath(uri);
-		path = path.replace(/\\/g, '/');
-		return path;
-	});
-	context.subscriptions.push(disposable);
+	let uri;
+	if (vscode.window.activeTextEditor) {
+		uri = vscode.window.activeTextEditor.document.uri;
+	}
+	if (!uri) {
+		vscode.window.showErrorMessage('Cannot get path (no file is opened?)');
+		return;
+	}
+	return uri;
+}
+
+function registerTaskCommand(context: vscode.ExtensionContext, commandFunction: (uri: vscode.Uri) => string) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'wsl-path.' + commandFunction.name,
+			function (uri) {
+				uri = getActiveDocumentUri();
+				if (!uri) { return; }
+				return commandFunction(uri);
+			}
+		)
+	);
+}
+
+function getRelativePath(uri: vscode.Uri): string {
+	var path = vscode.workspace.asRelativePath(uri);
+	path = path.replace(/\\/g, '/');
+	return path;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	registerTaskCommand(context, getRelativePath);
 }
 
 export function deactivate() { }
